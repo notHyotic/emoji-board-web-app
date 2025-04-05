3749933e4fd3ce00404c02811396683ce2e21410
notHyotic <hy0t1c1@gmail.com>
2025-04-05 15:45:00 -0400

    fix color

---
+++ components/CollectionTitle.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+	title: string
+	emojiSVG: string
+}>()
+</script>
+
+<template>
+	<a
+		href="/smileys-emotion"
+		class="flex h-9 w-full items-center rounded-md px-3 py-2 text-amber-200 hover:bg-gray-200 dark:hover:bg-gray-800"
+	>
+		<span class="text-md whitespace-nowrap">{{ title }}</span>
+	</a>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,30 @@
 <script setup lang="ts">
-import logoUrl from '~/assets/img/flushedbread.png'
+import logoUrl from "~/assets/img/flushedbread.png"
 </script>
 
 <template>
 	<div class="flex h-screen w-screen flex-row">
 		<div id="sidebar" class="h-full w-[20%] bg-amber-600">
-			<div id="app-banner" class="flex flex-col border-solid border-b-2 border-b-amber-300">
-				<div class="font-extrabold text-amber-300 text-5xl p-2">
+			<div
+				id="app-banner"
+				class="flex flex-col border-b-2 border-solid border-b-amber-300"
+			>
+				<div class="p-2 text-5xl font-extrabold text-amber-300">
 					EMOJI BOARD
 				</div>
 				<div class="flex justify-center">
-					<img :src="logoUrl"
-						class="h-[80px] w-[80px]" 
-					/>
+					<img :src="logoUrl" class="h-[80px] w-[80px]" />
 				</div>
+			</div>
+
+			<div id="collections" class="space-y-1 border-b-2 border-b-amber-300 p-6">
+				<div
+					class="text-bamber-300 block pb-2 text-xs tracking-widest uppercase dark:text-amber-950"
+				>
+					Collections
+				</div>
+
+				<CollectionTitle title="Smileys & Emotion" />
 			</div>
 		</div>
 
