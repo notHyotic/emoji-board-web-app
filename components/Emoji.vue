0ffeeeeda64390c6a94b02cde56af6d297b7c4a3
notHyotic <hy0t1c1@gmail.com>
2025-04-05 05:39:41 -0400

    add copy functionality to emoji

--- components/Emoji.vue
+++ components/Emoji.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
 	emojiName: string
 	imgUrl: string
 }>()
 
 const emojiHeight = "60px"
+
+// Function to copy the image URL to the clipboard
+const copyImageToClipboard = async () => {
+    await navigator.clipboard.writeText(props.imgUrl)
+}
 </script>
 
 <template>
-	<div class="rounded-2xl border-2 border-solid border-black bg-amber-200">
+	<div 
+        class="rounded-2xl border-2 border-solid border-black bg-amber-200"
+        @click="copyImageToClipboard"
+    >
 		<div class="m-2 flex flex-col">
 			<img class="" :src="imgUrl" :height="emojiSize" :width="emojiHeight" />
 			<div class="text-center font-bold">
