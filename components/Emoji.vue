900cbd5631dac2d5c1216cae4c7e0b35b766136f
notHyotic <hy0t1c1@gmail.com>
2025-04-05 05:42:48 -0400

    a

--- components/Emoji.vue
+++ components/Emoji.vue
@@ -8,15 +8,15 @@ const emojiHeight = "60px"
 
 // Function to copy the image URL to the clipboard
 const copyImageToClipboard = async () => {
-    await navigator.clipboard.writeText(props.imgUrl)
+	await navigator.clipboard.writeText(props.imgUrl)
 }
 </script>
 
 <template>
-	<div 
-        class="rounded-2xl border-2 border-solid border-black bg-amber-200"
-        @click="copyImageToClipboard"
-    >
+	<div
+		class="rounded-2xl border-2 border-solid border-black bg-amber-200"
+		@click="copyImageToClipboard"
+	>
 		<div class="m-2 flex flex-col">
 			<img class="" :src="imgUrl" :height="emojiSize" :width="emojiHeight" />
 			<div class="text-center font-bold">
