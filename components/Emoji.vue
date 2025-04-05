119b106866c6b45230aca35d9e8b512ad86fb6fd
notHyotic <hy0t1c1@gmail.com>
2025-04-05 05:15:48 -0400

    create emoji component

---
+++ components/Emoji.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+	emojiName: string
+	imgUrl: string
+}>()
+
+const emojiHeight = "60px"
+</script>
+
+<template>
+	<div class="border-solid border-black border-2">
+		<img :src="imgUrl" :height="emojiSize" :width="emojiHeight" />
+		<div>
+			{{ emojiName }}
+		</div>
+	</div>
+</template>
